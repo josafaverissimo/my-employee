33ef407c0a625e9f5738890276e19b4e526aa214
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-10-12 06:26:38 -0300

    feat: form filter to my-table-component created

--- frontend/src/app/components/my-table/my-table.component.ts
+++ frontend/src/app/components/my-table/my-table.component.ts
@@ -11,9 +11,25 @@ export class MyTableComponent implements OnInit {
   @Input() rows: string[][] = []
   @Input() actions: TableActionButton[] = []
   @Input() onClickRow = (row: string[]) => {}
+  @Input() hasFilter: boolean = true
   isActionsEmpty: boolean = true
+  allRows: string[][] = []
+  filterValue: string = ''
 
   ngOnInit(): void {
     this.isActionsEmpty = this.actions.length === 0
+  }
+
+  ngOnChanges() {
+    this.allRows = this.rows
+  }
+
+  filterForm() {
+    this.rows = this.allRows.filter(row => row.some(cell => cell.indexOf(this.filterValue) !== -1))
+  }
+
+  clearFilterForm() {
+    this.filterValue = ''
+    this.rows = this.allRows
   }
 }
